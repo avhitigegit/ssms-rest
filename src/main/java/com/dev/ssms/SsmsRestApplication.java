8e4477460bbeb9d1b8ed1df58ef6bcc4eff62efb
DESKTOP-FDTUA0L\scit <amritha.h@tailwindsw.com>
2021-09-12 23:04:25 +0530

    Initial Commit to Master

---
+++ src/main/java/com/dev/ssms/SsmsRestApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ssms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmsRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmsRestApplication.class, args);
+    }
+
+}
